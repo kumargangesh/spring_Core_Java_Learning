6af7a785083cb9fba5991ca493c5e8d855b8f25d
KumarGangesh123 <116815600+KumarGangesh123@users.noreply.github.com>
2023-03-23 09:23:51 +0530

    Update methods.java

--- methods.java
+++ methods.java
@@ -1,10 +1,8 @@
 
 public class methods{
-    public static void main(String []argument){
-        
-        public void showName(String name){              // normal method to implement spring methods
-            System.out.print("\nHello :\t"+name);
-        }
-        
+    
+    public void showName(String name){              // normal method to implement spring methods
+        System.out.print("\nHello :\t"+name);
     }
+    
 }
