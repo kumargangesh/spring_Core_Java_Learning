1908aec070656da9b0668fc8e6db57da9e3b41f6
KumarGangesh123 <116815600+KumarGangesh123@users.noreply.github.com>
2023-03-23 09:22:15 +0530

    Create methods.java

---
+++ methods.java
@@ -0,0 +1,10 @@
+
+public class methods{
+    public static void main(String []argument){
+        
+        public void showName(String name){              // normal method to implement spring methods
+            System.out.print("\nHello :\t"+name);
+        }
+        
+    }
+}
